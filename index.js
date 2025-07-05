8120b24e09a6aee55847b8567329fcb9b2083df6
hikkyne <101345142+hikkyne@users.noreply.github.com>
2025-07-06 00:15:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js');
+const keep_alive = require('./keep_alive.js')
 
-// Tạo bot
+// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
-// Khi bot đã sẵn sàng
-bot.on("ready", () => {
-  console.log(`✅ Bot đã đăng nhập với tên: ${bot.user.username}`);
-
-  // Thiết lập trạng thái
-  bot.editStatus("idle", {
-    name: "...",
-    type: 3 // Watching
-  });
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
 });
 
-// Bắt lỗi
-bot.on("error", console.error);
-
-// Kết nối bot
-bot.connect();
+bot.connect(); // Get the bot to connect to Discord
